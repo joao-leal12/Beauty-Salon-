ca567e26d6539b43734054ccb3a3f08fb389d4d4
João <joaovictorleal080415@gmail.com>
2022-10-14 10:47:17 -0300

    Alterações

--- src/controllers/scripts/modulesJs/action.js
+++ src/controllers/scripts/modulesJs/action.js
@@ -1,14 +1,7 @@
 
 
 
-export function ActionsMenu(cards){ 
-    
-    ActiveMenuOnClick(); 
-    closeMenuOnClick() ; 
-    CreateElement(cards);   
-    onScroll();   
-
-} 
+ 
 
 
 
@@ -162,10 +155,189 @@ function CreateElement(cards){
 }
 
 
-function Slider(){ 
+// function Sliders(){ 
+
+//     let state = { 
+
+//         stateInitial:  0, 
+//         lastposition: 0, 
+//         currentSlideposition: 0, 
+//         stateMoveUp: -567  
+    
+//     } 
+
+//      SetListeners()
+
+
+// } 
+ 
+class SliderAction { 
+
+    constructor() { 
+
+        this.stateInitial=  0;
+        this.lastposition= 0;
+        this.currentSlideposition= 0;
+        this.stateMoveUp= -567; 
+
+    }
+
+    Slider() { 
+
+        return document.querySelector('[data-slide="slider"]')
+
+    }
+    SetListeners(){ 
+        const slider = this.Slider(); 
+    
+        slider.addEventListener('mousedown', this.onMouseDown);
+        slider.addEventListener('mouseup', this.onMouseUp);  
+        slider.addEventListener('mouseleave', this.onMouseLeave)
+        slider.addEventListener('dragstart', this.DragStart); 
+    
+    } 
+
+    onMouseDown(e){ 
+        
+        this.stateInitial = e.clientX 
+        this.currentSlideposition = e.clientX - this.lastposition; 
+        e.currentTarget.addEventListener('mousemove', this.onMouseMove)  
+    } 
+    
+    onMouseMove(e){ 
+        
+        let position;  
+        let validation = e.clientX - this.currentSlideposition
+
+        if(validation > -800 && validation < 100){ 
+        position = e.clientX - this.currentSlideposition; 
+        SlideItem(position, e,0);   
+        } 
+     }
+
+
+     onMouseUp(e){  
+       
+        let validation = e.clientX - this.currentSlideposition;  
+        this.stateMoveUp  = this.stateMoveUp == -567? 5 : -567
+
+ 
+        if(validation < -567){
+                position == position == null? -567 : position; 
+            SlideItem(-567, e, 100)
+            
+            }else if(validation > 5){ 
+                
+                SlideItem(5, e, 100)
+            }else if(validation < 5 && validation > -567){ 
+                
+                
+                SlideItem(this.stateMoveUp, e, 100)   
+            
+            }
+            e.currentTarget.removeEventListener('mousemove', this.onMouseMove);
+
+    }
+    onMouseLeave(e){  
+        e.currentTarget.removeEventListener('mousemove', this.onMouseMove); 
+    }
+     DragStart(e){ 
+
+        e.preventDefault();   
+    } 
+    SlideItem(position, item, transition ){ 
+        this.lastposition = position; 
+        item.currentTarget.style.transition = `${transition}ms`
+        item.currentTarget.style.transform = `translateX(${position}px)` 
+    }
+
+}
+
+export const SlidesAction = new SliderAction(); 
+
+
+
+
+
+
+
+// function Slider(){return document.querySelector('[data-slide="slider"]');}
+// function SetListeners(){ 
+//     const slider = Slider(); 
+
+//     slider.addEventListener('mousedown', onMouseDown);
+//     slider.addEventListener('mouseup', onMouseUp);  
+//     slider.addEventListener('mouseleave', onMouseLeave)
+//     slider.addEventListener('dragstart', DragStart); 
+
+// } 
+
+//     function onMouseDown(e){ 
+        
+//         state.stateInitial = e.clientX 
+//         state.currentSlideposition = e.clientX - state.lastposition; 
+//         e.currentTarget.addEventListener('mousemove', onMouseMove)  
+//     }
+//     function SlideItem(position, item, transition ){ 
+//         state.lastposition = position; 
+//         item.currentTarget.style.transition = `${transition}ms`
+//         item.currentTarget.style.transform = `translateX(${position}px)` 
+//     }
+//     function onMouseMove(e){ 
+//         let position;  
+//         let validation = e.clientX - state.currentSlideposition
+
+//         if(validation > -800 && validation < 100){ 
+//         position = e.clientX - state.currentSlideposition; 
+//         SlideItem(position, e,0);   
+//         }
+
+
+//     }
+//     function onMouseUp(e){  
+       
+//         let validation = e.clientX - state.currentSlideposition;  
+//             state.stateMoveUp  = state.stateMoveUp == -567? 5 : -567
+
+
+//             console.log(validation); 
+//         if(validation < -567){
+//                 position == position == null? -567 : position; 
+//             SlideItem(-567, e, 100)
+            
+//             }else if(validation > 5){ 
+                
+//                 SlideItem(5, e, 100)
+//             }else if(validation < 5 && validation > -567){ 
+                
+                
+//                 SlideItem(state.stateMoveUp, e, 100)   
+            
+//             }
+
+//             console.log(validation)
+
+
+//             e.currentTarget.removeEventListener('mousemove', onMouseMove);
+
+//     }
+//     function onMouseLeave(e){  
+//         e.currentTarget.removeEventListener('mousemove', onMouseMove); 
+//     }
+
+//     function DragStart(e){ 
+
+//         e.preventDefault();   
+//     } 
+
 
     
+export function ActionsMenu(cards){ 
+    
+    ActiveMenuOnClick(); 
+    closeMenuOnClick() ; 
+    CreateElement(cards);   
+    onScroll();    
+  
 
 } 
-
-
