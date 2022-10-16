e9ee5fa09a50f21adf42f5f1e26236428679fed2
João <joaovictorleal080415@gmail.com>
2022-10-16 13:12:33 -0300

    Novas funcionalidades

--- src/controllers/scripts/modulesJs/action.js
+++ src/controllers/scripts/modulesJs/action.js
@@ -155,180 +155,86 @@ function CreateElement(cards){
 }
 
 
-// function Sliders(){ 
 
-//     let state = { 
+function startSlider() { 
 
-//         stateInitial:  0, 
-//         lastposition: 0, 
-//         currentSlideposition: 0, 
-//         stateMoveUp: -567  
+        let state = { 
+            stateInitial:  0,
+            lastposition: 0,
+            currentSlideposition: 0,
+            stateMoveUp: -567, 
+        }
     
-//     } 
-
-//      SetListeners()
-
-
-// } 
- 
-class SliderAction { 
-
-    constructor() { 
-
-        this.stateInitial=  0;
-        this.lastposition= 0;
-        this.currentSlideposition= 0;
-        this.stateMoveUp= -567; 
-
-    }
-
-    Slider() { 
-
-        return document.querySelector('[data-slide="slider"]')
-
-    }
-    SetListeners(){ 
-        const slider = this.Slider(); 
+       function Slider(){return document.querySelector('[data-slide="slider"]');}
     
-        slider.addEventListener('mousedown', this.onMouseDown);
-        slider.addEventListener('mouseup', this.onMouseUp);  
-        slider.addEventListener('mouseleave', this.onMouseLeave)
-        slider.addEventListener('dragstart', this.DragStart); 
-    
-    } 
+        function SetListeners(){ 
+            const slider = Slider(); 
 
-    onMouseDown(e){ 
-        
-        this.stateInitial = e.clientX 
-        this.currentSlideposition = e.clientX - this.lastposition; 
-        e.currentTarget.addEventListener('mousemove', this.onMouseMove)  
-    } 
-    
-    onMouseMove(e){ 
-        
-        let position;  
-        let validation = e.clientX - this.currentSlideposition
+            slider.addEventListener('mousedown', onMouseDown);
+            slider.addEventListener('mouseup', onMouseUp);  
+            slider.addEventListener('mouseleave', onMouseLeave)
+            slider.addEventListener('dragstart', DragStart); 
 
-        if(validation > -800 && validation < 100){ 
-        position = e.clientX - this.currentSlideposition; 
-        SlideItem(position, e,0);   
         } 
-     }
+
+        function onMouseDown(e){ 
+        
+            state.stateInitial = e.clientX 
+            state.currentSlideposition = e.clientX - state.lastposition; 
+            e.currentTarget.addEventListener('mousemove', onMouseMove)  
+        }
+        function SlideItem(position, item, transition ){ 
+            state.lastposition = position; 
+            item.currentTarget.style.transition = `${transition}ms`
+            item.currentTarget.style.transform = `translateX(${position}px)` 
+        }
+            function onMouseMove(e){ 
+              let position;  
+                let validation = e.clientX - state.currentSlideposition
+                
+                if(validation > -800 && validation < 100){ 
+                position = e.clientX - state.currentSlideposition;  
+                SlideItem(position, e,0);   
+                }
 
 
-     onMouseUp(e){  
-       
-        let validation = e.clientX - this.currentSlideposition;  
-        this.stateMoveUp  = this.stateMoveUp == -567? 5 : -567
-
- 
-        if(validation < -567){
-                position == position == null? -567 : position; 
-            SlideItem(-567, e, 100)
-            
-            }else if(validation > 5){ 
-                
-                SlideItem(5, e, 100)
-            }else if(validation < 5 && validation > -567){ 
-                
-                
-                SlideItem(this.stateMoveUp, e, 100)   
-            
             }
-            e.currentTarget.removeEventListener('mousemove', this.onMouseMove);
+        function onMouseUp(e){  
+            e.currentTarget.removeEventListener('mousemove', onMouseMove);
+        
+            let validation = e.clientX - state.currentSlideposition;  
+                state.stateMoveUp  = state.stateMoveUp == -567? 5 : -567
 
-    }
-    onMouseLeave(e){  
-        e.currentTarget.removeEventListener('mousemove', this.onMouseMove); 
-    }
-     DragStart(e){ 
+            if(validation < -567){
+            
+                SlideItem(-567, e, 100)
+            
+                }else if(validation > 5){ 
+                
+                    SlideItem(5, e, 100)
+                }else if(validation < 5 && validation > -567){ 
+                
+                    SlideItem(state.stateMoveUp, e, 100)   
+            
+                }
 
-        e.preventDefault();   
-    } 
-    SlideItem(position, item, transition ){ 
-        this.lastposition = position; 
-        item.currentTarget.style.transition = `${transition}ms`
-        item.currentTarget.style.transform = `translateX(${position}px)` 
-    }
+
+            }
+            function onMouseLeave(e){  
+                e.currentTarget.removeEventListener('mousemove', onMouseMove); 
+            }
+
+            function DragStart(e){ 
+
+                e.preventDefault();   
+            }  
+
+
+            SetListeners(); 
+
 
 }
 
-export const SlidesAction = new SliderAction(); 
-
-
-
-
-
-
-
-// function Slider(){return document.querySelector('[data-slide="slider"]');}
-// function SetListeners(){ 
-//     const slider = Slider(); 
-
-//     slider.addEventListener('mousedown', onMouseDown);
-//     slider.addEventListener('mouseup', onMouseUp);  
-//     slider.addEventListener('mouseleave', onMouseLeave)
-//     slider.addEventListener('dragstart', DragStart); 
-
-// } 
-
-//     function onMouseDown(e){ 
-        
-//         state.stateInitial = e.clientX 
-//         state.currentSlideposition = e.clientX - state.lastposition; 
-//         e.currentTarget.addEventListener('mousemove', onMouseMove)  
-//     }
-//     function SlideItem(position, item, transition ){ 
-//         state.lastposition = position; 
-//         item.currentTarget.style.transition = `${transition}ms`
-//         item.currentTarget.style.transform = `translateX(${position}px)` 
-//     }
-//     function onMouseMove(e){ 
-//         let position;  
-//         let validation = e.clientX - state.currentSlideposition
-
-//         if(validation > -800 && validation < 100){ 
-//         position = e.clientX - state.currentSlideposition; 
-//         SlideItem(position, e,0);   
-//         }
-
-
-//     }
-//     function onMouseUp(e){  
-       
-//         let validation = e.clientX - state.currentSlideposition;  
-//             state.stateMoveUp  = state.stateMoveUp == -567? 5 : -567
-
-
-//             console.log(validation); 
-//         if(validation < -567){
-//                 position == position == null? -567 : position; 
-//             SlideItem(-567, e, 100)
-            
-//             }else if(validation > 5){ 
-                
-//                 SlideItem(5, e, 100)
-//             }else if(validation < 5 && validation > -567){ 
-                
-                
-//                 SlideItem(state.stateMoveUp, e, 100)   
-            
-//             }
-
-//             console.log(validation)
-
-
-//             e.currentTarget.removeEventListener('mousemove', onMouseMove);
-
-//     }
-//     function onMouseLeave(e){  
-//         e.currentTarget.removeEventListener('mousemove', onMouseMove); 
-//     }
-
-//     function DragStart(e){ 
-
-//         e.preventDefault();   
-//     } 
 
 
     
@@ -338,6 +244,6 @@ export function ActionsMenu(cards){
     closeMenuOnClick() ; 
     CreateElement(cards);   
     onScroll();    
-  
+    startSlider()
 
 } 
